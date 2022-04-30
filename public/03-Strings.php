b875840f5724134363a7f6c54113111aa2358a00
Caglar Kaya <C.kaya@Intermax.nl>
2022-04-30 13:33:25 +0200

    end of strings

--- public/03-Strings.php
+++ public/03-Strings.php
@@ -1,3 +1,16 @@
+<!--Strings-->
 <?php
-// Write your code below:
-echo "Hello, World!";
+echo "Hello, World!";?>
+
+<!--Escape Sequences-->
+<?php
+echo "1. learn PHP";
+echo "\n2. learn Laravel";
+echo "\n3. Learn to have \"fun\"";?>
+
+<!--String Concatenation-->
+<?php
+echo "Code"."cademy";
+echo "\nMy name is:"." Caglar";
+echo "\n"."tur"."duck"."en";?>
+
