9c69c6022db77ea7404bb6d4f0c99904e1a1463c
Caglar Kaya <C.kaya@Intermax.nl>
2022-05-01 14:30:45 +0200

    built-in-functions

---
+++ public/07-Built-in-Functions.php
@@ -0,0 +1,12 @@
+<!--BUILT-IN PHP FUNCTIONS-->
+<?php
+echo("This works!\n"); // This works!
+
+echo "This also works!\n"; // This also works!
+
+// echo("This would NOT work", "\n");
+
+echo "Buuuut!", " ", "This", " ", "does!", "\n"; // Buuuut! This does!
+?>
+
+<!--Working with Variables-->
