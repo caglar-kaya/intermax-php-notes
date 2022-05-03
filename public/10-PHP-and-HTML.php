d48444bf3aa3958746b9b566766602ec8e5ca4ae
Caglar Kaya <C.kaya@Intermax.nl>
2022-05-03 13:06:13 +0200

    php and html

--- public/10-PHP-and-HTML.php
+++ public/10-PHP-and-HTML.php
@@ -1,1 +1,38 @@
+<html>
+<body>
+<h1>PHP and HTML</h1>
+<p>This is HTML</p>
 <?php
+echo "<p>This is PHP</p>";
+?>
+
+<p>*****************************************************************************************************************</p>
+
+<?php
+$about_me = [
+    "name" => "Aisle Nevertell",
+    "birth_year" => 1902,
+    "favorite_food" => "pizza"
+];
+
+function calculateAge ($person_arr){
+    $current_year = date("Y");
+    $age = $current_year - $person_arr["birth_year"];
+    return $age;
+}
+?>
+
+<h1>Welcome!</h1>
+<h2>About me:</h2>
+
+<?php
+#Add your code here
+echo "<h3>Name: {$about_me["name"]}</h3>";
+echo "<p>Age: ".calculateAge($about_me)."</p>";
+echo "<div>Favorite food: {$about_me["favorite_food"]}</div>"
+?>
+
+<p>*****************************************************************************************************************</p>
+
+</body>
+</html>
